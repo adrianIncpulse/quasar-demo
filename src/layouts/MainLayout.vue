c137c471d82f85687897267f3ac71ed3dd452991
Adrian <adrian@incpulses.com>
2023-06-11 19:49:21 -0600

    primer push

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,105 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-green-6">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered mobile>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <div
+      class="full-width row justify-evenly bg-black text-white q-py-lg items end"
+      style="min-height: 300px"
+    >
+      <div class="col-3">Contactanos</div>
+      <div class="col-3">Redes sociales</div>
+      <div class="col-3">Info</div>
+
+      <div class="col-10 text-center" style="height: 30px">
+        DERECHOS RESERVADOS A QUIEN CORRESPONDA 2023
+      </div>
+    </div>
+  </q-layout>
+</template>
+
+<script setup>
+import EssentialLink from "components/EssentialLink.vue";
+import { ref } from "vue";
+
+const essentialLinks = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-page class="bg-grey-12">
+    <q-responsive :ratio="16 / 9" style="max-height: 100vh">
+      <q-carousel animated v-model="slide" arrows navigation infinite>
+        <q-carousel-slide
+          :name="1"
+          img-src="https://cdn.quasar.dev/img/mountains.jpg"
+        />
+        <q-carousel-slide
+          :name="2"
+          img-src="https://cdn.quasar.dev/img/parallax1.jpg"
+        />
+        <q-carousel-slide
+          :name="3"
+          img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+        />
+        <q-carousel-slide
+          :name="4"
+          img-src="https://cdn.quasar.dev/img/quasar.jpg"
+        />
+        <q-carousel-slide :name="0" :img-src="imageOmnilife" />
+      </q-carousel>
+    </q-responsive>
+
+    <q-responsive :ratio="16 / 9" style="max-height: 100vh" class="q-mt-xl">
+      <div class="row fit justify-center items-center">
+        <div class="col-10 bg-white shadow-1" style="border: 1px solid #fdfdfd">
+          <div class="row full-width">
+            <div style="width: 50%" class="q-pa-md q-pl-lg">HOLA OMNIPLUS</div>
+            <div style="width: 50%">
+              <q-img :src="omniplus" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </q-responsive>
+
+    <q-responsive :ratio="16 / 9" style="max-height: 100vh">
+      <div class="row fit justify-center items-center">
+        <div class="col-10 bg-white shadow-1" style="border: 1px solid #fdfdfd">
+          <div class="row full-width">
+            <div style="width: 50%">
+              <q-img :src="aqtua" />
+            </div>
+            <div style="width: 50%" class="q-pa-md q-pl-lg">HOLA AQTUA</div>
+          </div>
+        </div>
+      </div>
+    </q-responsive>
+
+    <q-responsive :ratio="16 / 9" style="max-height: 100vh">
+      <div class="row fit justify-center items-center">
+        <div class="col-10 bg-white shadow-1" style="border: 1px solid #fdfdfd">
+          <div class="row full-width">
+            <div style="width: 50%">
+              <q-img :src="power" />
+            </div>
+            <div style="width: 50%" class="q-pa-md q-pl-lg">HOLA MUNDO</div>
+          </div>
+        </div>
+      </div>
+    </q-responsive>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import imageOmnilife from "src/assets/banners/omni.jpg";
+import power from "src/assets/productos/power.png";
+import omniplus from "src/assets/productos/omniplus.jpg";
+import aqtua from "src/assets/productos/aqtua.jpg";
+
+const slide = ref(0);
+</script>
